1bc6f6330564505ceae835a7d94128f080f64025
chlenslonika282 <132747054+chlenslonika282@users.noreply.github.com>
2023-05-06 12:27:55 +0300

    да

---
+++ igra.py
@@ -0,0 +1,28 @@
+from pygame import *
+from random import randint
+
+win_width = 700
+win_height = 500
+window = display.set_mode((win_width, win_height))
+display.set_caption('ПИНГ-ПОНГ')
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, w, h):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (w, h))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image,(self.rect.x, self.rect.y))
+
+class Enemy(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        global lost
+        if self.rect.y > 500:
+            self.rect.y = 0
+            self.rect.x = randint(25, 675) 
+            lost = lost + 1S
+
